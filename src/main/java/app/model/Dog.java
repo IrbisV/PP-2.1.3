7ec50520c6e40452c9288752a9d00eee09a6092f
Dmitry Vanyukov <ekoetnotur@yandex.ru>
2022-09-29 13:06:22 +0300

    format fix

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Component;
 @Component
 public class Dog extends Animal {
     @Override
-    public String toString() {return "Im a Dog";}
+    public String toString() { return "Im a Dog"; }
 }
